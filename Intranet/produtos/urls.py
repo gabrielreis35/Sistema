498ff6ca206d277e0461bd563bf166166cbd00be
Gabriel Reis <gabrielreis@vixdynamics.com.br>
2021-09-09 13:26:44 -0300

    update all

--- Intranet/produtos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Produtos, name = 'Produtos'),
-    path('produto-<int:id>', views.viewProduto, name = 'View_Produtos'),
-    path('addproduto', views.addProduto, name = 'add_Produtos'),
-]
