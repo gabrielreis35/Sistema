d432b370bc2fdefb577159df51d2a1f15c65a82b
gabrielreis35 <gabrielreis@vixdynamics.com.br>
2021-10-14 14:28:20 -0300

    Update Settings Jquery

--- Intranet/settings.py
+++ Intranet/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jquery',
+    'django-static-jquery',
     'crispy_forms',
     'debug_toolbar',
     'produtos',
