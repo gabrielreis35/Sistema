b57c8b2f522d4b023c027001263f928c873345b4
unknown <gabrielreis@vixdynamics.com.br>
2021-12-01 08:43:58 -0300

    Update Home

--- main/views.py
+++ main/views.py
@@ -10,9 +10,40 @@ from .models import Informacoes
 @login_required
 def Home(request):
     countProduct = Produto.objects.count()
-    tipProduct = Produto.objects.filter(tipoProduto__nome = 'Graniteira').count()
+    acessorio = Produto.objects.filter(tipoProduto__nome = 'Acessório').count()
+    carregadeira = Produto.objects.filter(tipoProduto__nome = 'Carregadeira').count()
+    chapaDesgaste = Produto.objects.filter(tipoProduto__nome = 'Chapa de desgaste').count()
+    chockBar = Produto.objects.filter(tipoProduto__nome = 'Chock bar').count()
+    dispFer = Produto.objects.filter(tipoProduto__nome = 'Dispositivos e ferramentas').count()
+    engate = Produto.objects.filter(tipoProduto__nome = 'Engate rápido').count()
+    escavadeira = Produto.objects.filter(tipoProduto__nome = 'Escavadeira').count()
+    ferramentaSolo = Produto.objects.filter(tipoProduto__nome = 'Ferramenta de penetração de solo').count()
+    garfo = Produto.objects.filter(tipoProduto__nome = 'Garfo').count()
+    graniteira = Produto.objects.filter(tipoProduto__nome = 'Graniteira').count()
+    lamina = Produto.objects.filter(tipoProduto__nome = 'Lâmina').count()
+    lanca = Produto.objects.filter(tipoProduto__nome = 'Lança').count()
+    ripper = Produto.objects.filter(tipoProduto__nome = 'Ripper').count()
+    segmento = Produto.objects.filter(tipoProduto__nome = 'Segmento').count()
+    vala = Produto.objects.filter(tipoProduto__nome = 'Vala').count()
+    servico = Produto.objects.filter(tipoProduto__nome = 'Serviço').count()
+    
     context = {
         'countProduct' : countProduct,
-        'tipProduct' : tipProduct
+        'acessorio' : acessorio,
+        'carregadeira' : carregadeira,
+        'chapaDesgaste' : chapaDesgaste,
+        'chockBar' : chockBar,
+        'graniteira' : graniteira,
+        'dispFer' : dispFer,
+        'engate' : engate,
+        'escavadeira' : escavadeira,
+        'ferramentaSolo' : ferramentaSolo,
+        'garfo' : garfo,
+        'lamina' : lamina,
+        'lanca' : lanca,
+        'ripper' : ripper,
+        'segmento' : segmento,
+        'vala' : vala,
+        'servico' : servico
     }
     return render(request, 'main/Home.html', context)
