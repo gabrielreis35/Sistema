f606d248c4c04f761028973e61b579bf6db77a47
unknown <gabrielreis@vixdynamics.com.br>
2022-01-04 15:00:53 -0300

    Update user model

--- departments/urls.py
+++ departments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateDepartment, HomeDepartment
+from .views import CreateDepartment, HomeDepartment, DeleteDepartment
 
 app_name = 'departments'
 
 urlpatterns = [
     path('', HomeDepartment, name = 'Home_Departments'),
     path('newDepartment', CreateDepartment, name = 'New_Department'),
+    path("deleteDepartment/<int:id>", DeleteDepartment, name="Delete_Department")
 ]
